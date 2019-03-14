19cf04cca02b1a2babc8d74908d7fea267e6e1a7
lujiaxing <lujiaxing@gmail.com>
2019-03-14 19:10:50 +0800

    i18n. fix bugs

--- imageupload/plugin.js
+++ imageupload/plugin.js
@@ -1,5 +1,5 @@
-(function (TinyMCEImageUpload) {
-    return new TinyMCEImageUpload();
+(function (ImageUpload) {
+    return new ImageUpload();
 })(
     function () {
         'use strict';
@@ -7,6 +7,8 @@
         var _this = this;
     
         this.global = tinymce.util.Tools.resolve('tinymce.PluginManager');
+		
+		this.i18n = tinymce.util.Tools.resolve('tinymce.util.I18n');
 
         this.commandRegister = function(editor){
             editor.addCommand('SelectAnImageAndUpload', function () {
@@ -21,7 +23,7 @@
 
             editor.ui.registry.addButton('imageupload', {
                 icon: "image",
-                tooltip: 'Choose one or several images from local storage',
+                tooltip: this.i18n.translate(['Choose one or several images from local storage']),
                 onAction: function () {
                   return editor.execCommand('SelectAnImageAndUpload');
                 }
@@ -29,63 +31,129 @@
         };
 
         this.selectLocalImages = function(editor) {
-            var dom = editor.dom;
-
-            var hiddenInputFileControl = document.createElement("input");
-
-            hiddenInputFileControl.setAttribute("type", "file");
-            hiddenInputFileControl.setAttribute("multiple", "multiple");
-            hiddenInputFileControl.setAttribute("accept", (editor.settings.allowUploadTypes || "image/jpg,image/jpeg,image/png,image/gif"));
-            hiddenInputFileControl.style.display = "none";
-
-
-            var onChangeEventMethod = null;
-            hiddenInputFileControl.addEventListener('change', onChangeEventMethod = function () {
-
-                var files = hiddenInputFileControl.files;
-
+  
+			var hiddenInputFileControl = 
+			$(editor.dom.createHTML('input', {
+				type: "file",
+				multiple: "multiple",
+				accept: (editor.settings.allowImageUploadTypes || this.defaultAllowImageUploadTypes),
+				style: "display: none",
+			}));
+			
+			hiddenInputFileControl.bind("change",
+				function(){
+					_this.onHiddenInputFileControlSelectionChanged(editor, this);
+				});
+			
+            hiddenInputFileControl.click();
+        };
+		
+		this.onHiddenInputFileControlSelectionChanged = function(editor, targetFileControl){
+			
+				var dom = editor.dom;
+				var maxFileSizeLimit = editor.settings.maxImageFileSize;
+                var files = targetFileControl.files;
+				
                 var formData = new FormData();
-
-                var curFile = null;
+				var timeoutFormConfig = 0;
+                var currFile = null;
+				
                 for(var i = 0; i < files.length; i++)
-                    formData.append("file" + i, files[i]);
-
-
+				{
+					currFile = files[i];
+					
+					if(maxFileSizeLimit && currFile.size > maxFileSizeLimit)
+					{
+						_this.alert(editor, "warning", this.i18n.translate(["File size limit exceeded: {0}", currFile.name]));
+						$(targetFileControl).unbind("change");
+						return;
+					}
+					
+                    formData.append("file" + i, currFile);
+				}
+				
+				
                 $.ajax({
                     url : editor.settings.upload_image_url,  
-                    type : "POST",  
+                    type : "POST",
+                    timeout: 
+						((editor.settings.imageUploadTimeout && !isNaN(timeoutFormConfig = parseInt(editor.settings.imageUploadTimeout))) ? 
+							timeoutFormConfig :  _this.defaultUploadTimeout),
                     data: formData,
+					timeout: 
+						((editor.settings.imageUploadTimeout && !isNaN(timeoutFormConfig = parseInt(editor.settings.imageUploadTimeout))) ? 
+							timeoutFormConfig :  _this.defaultUploadTimeout),
                     processData:false,
                     contentType:false,
                     complete: function(){
-                        hiddenInputFileControl.removeEventListener("change", onChangeEventMethod);
+                        $(targetFileControl).unbind("change");
                     },
                     success : function(resp) {
 
-                        if (resp && resp.errno == 0 && resp.data) {
+                        if (resp) {
 
-                            editor.focus();
+							if(resp.resultCode == 0)
+							{						
+								editor.focus();
 
-                            for(var j = 0; j < resp.data.length; j++){
-                                editor.selection.setContent(dom.createHTML('img', {src: 
-                                    (editor.settings.image_url_prefix || '') + resp.data[j]}));
+								for(var j = 0; j < resp.data.length; j++)
+                                    editor.selection.setContent(dom.createHTML('img', {src: 
+                                        (editor.settings.image_url_prefix || '') + resp.data[j]}));
                             }
-                            
+							else
+							{
+								var msg = resp.message;
+								
+								if(!msg && editor.settings.imageUploadErrorMessages){
+									msg = editor.settings.imageUploadErrorMessages[resp.resultCode];
+								}
+								else{
+									_this.errorMessages[resp.resultCode];
+								}
+								
+								
+								_this.alert(editor, "error", msg || _this.i18n.translate(["Unknown error"]));
+								
+								editor.windowManager.open()
+							}
+							
                         }
-                }});
+                }, error: function(xhr, textStatus, errorThrown){
+					
+					_this.alert(editor, "error", (textStatus || _this.i18n.translate(["Internal server error"])));
+					
+				}});
 
-            });
+		};
+		
+		this.alert = function(editor, type, msg){
+			
+			editor.notificationManager.close();
+			
+			editor.notificationManager.open({
+			  text: msg,
+			  type: type,
+			  timeout: 3000
+			});
+			
+		};
 
-            hiddenInputFileControl.click();
-        };
-
-        
+		this.defaultAllowImageUploadTypes = "image/jpeg,image/png,image/gif";
+		this.defaultUploadTimeout = 120000; //Milliseconds
+		this.errorMessages = [
+			"Upload succeeded",
+			"Nothing to upload",
+			"Unsupported image format",
+			"File size limit exceeded",
+			"Not enough disk space",
+			"Permission denied"
+		];
         this.Commands = { register: this.commandRegister };
         this.Buttons = { register: this.buttonRegister };
           
         this.global.add('imageupload', function (editor) {
-            _this.Commands.register(editor);
-            _this.Buttons.register(editor);
+            _this.Commands.register.call(_this, editor);
+            _this.Buttons.register.call(_this, editor);
         });
 
 
